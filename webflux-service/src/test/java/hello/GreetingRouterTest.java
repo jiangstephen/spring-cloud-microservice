8c1b95ad191dddacb1e887475c24aa3f940cda12
sjiang <jiangstephen@hotmail.com>
2019-01-03 18:18:06 -0500

    added the integration test to maven

--- webflux-service/src/test/java/hello/GreetingRouterTest.java
+++ webflux-service/src/test/java/hello/GreetingRouterTest.java
@@ -1,18 +1,19 @@
 package hello;
 
 
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 @RunWith(SpringRunner.class)
 //  We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Ignore
 public class GreetingRouterTest {
 
 	// Spring Boot will create a `WebTestClient` for you,
